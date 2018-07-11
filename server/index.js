3e263dc0b8f94aa0dec1f4bc8b3f12c547e55b76
Ivan5 <converse_ivan@hotmail.com>
2018-07-10 19:00:04 -0500

    Add schema db to the server using monk and joi modules

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const messages = require('./db/messages');
+
 
 const app = express();
 
@@ -14,6 +16,25 @@ app.get('/',(req,res)=>{
     message: 'Full stack meesage Board'
   });
 });
+
+app.get('/messages',(req,res)=>{
+  messages.getAll()
+    .then((messages) => {
+      res.json(messages);
+    })
+});
+
+app.post('/messages',(req,res)=>{
+  console.log(req.body);
+  messages.create(req.body)
+    .then((message)=>{
+      res.json(message);
+    }).catch((error) => {
+      res.status(500);
+      res.json(error);
+    })
+});
+
 
 const port = process.env.PORT || 3000;
 
